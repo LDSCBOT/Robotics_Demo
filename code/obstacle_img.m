0bcf97e9186dc6f25065265ed27475e705f4e326
sheng chieh <jack81121@gmail.com>
2016-09-18 16:25:13 +0800

    ver 1

---
+++ code/obstacle_img.m
@@ -0,0 +1,37 @@
+%% Creating map for A* alogrithm
+%% Input: Color patches' centroid point
+%% Output: 48x64 Binary Image
+function pic_b = obstacle_img(centroid_1, centroid_2)
+
+% create a empty map
+pic_b = zeros(48,64);
+
+% bump location
+bump = [23, 32];
+
+% bump and car
+for x = 1:48
+    for y = 1:64
+        if sqrt((x-bump(1))^2+(y-bump(2))^2) <= 3.5
+            pic_b(x,y) = 1;
+        end
+        
+        if sqrt((x-centroid_1(1))^2+(y-centroid_1(2))^2) <= 3.5
+            pic_b(x,y) = 1;
+        end
+        
+        if sqrt((x-centroid_2(1))^2+(y-centroid_2(2))^2) <= 3.5
+            pic_b(x,y) = 1;
+        end
+    end
+end
+
+% wall
+pic_b(1:19,1) = 1;
+pic_b(29:48,1) = 1;
+pic_b(1:19,64) = 1;
+pic_b(29:48,64) = 1;
+pic_b(1,:) = 1;
+pic_b(48,:) = 1;
+
+end
